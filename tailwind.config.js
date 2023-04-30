b3511f7314cd1e9dca510db01595c1fa61567bc9
Decio Neto <deciom.neto10@gmail.com>
2023-04-29 23:27:55 -0300

    feat: add tailwind color variables

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,8 +4,23 @@ export default {
   theme: {
     extend: {
       colors: {
-        primary: 'rgb(59 130 246)',
-        secondary: 'rgb(209 213 219)'
+        primary: 'rgb(var(--r-primary) / <alpha-value>)',
+        secondary: 'hsl(var(--r-secondary) / <alpha-value>)',
+
+        'text-light': 'rgb(var(--r-black-cozy) / <alpha-value>)',
+        'text-dark': 'rgb(var(--r-white-cozy) / <alpha-value>)',
+
+        'bg-light': 'rgb(var(--r-white) / <alpha-value>)',
+        'bg-dark': 'rgb(var(--r-black) / <alpha-value>)',
+
+        'bg-btn-light': 'rgb(var(--r-black-soft) / <alpha-value>)',
+        'bg-btn-dark': 'rgb(var(--r-white-soft) / <alpha-value>)',
+
+        'bg-btn-light-hover': 'rgb(var(--r-black-mute) / <alpha-value>)',
+        'bg-btn-dark-hover': 'rgb(var(--r-white-mute) / <alpha-value>)',
+
+        'footer-light': 'rgb(var(--r-black-mute) / <alpha-value>)',
+        'footer-dark': 'rgb(var(--r-white-mute) / <alpha-value>)'
       }
     }
   }
