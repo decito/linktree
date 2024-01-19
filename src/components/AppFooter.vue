a880f0619b0316c526f3b932dce2235ffae7406b
Decio Neto <deciom.neto10@gmail.com>
2024-01-19 18:49:58 -0300

    style: update prettier rules

--- src/components/AppFooter.vue
+++ src/components/AppFooter.vue
@@ -1,10 +1,6 @@
 <template>
   <footer class="w-full pb-4 pt-16 text-center">
-    <a
-      href="https://linkedin.com/in/desceoneto"
-      target="_blank"
-      class="hover:underline"
-    >
+    <a href="https://linkedin.com/in/desceoneto" target="_blank" class="hover:underline">
       <i class="fab fa-linkedin"></i>
 
       <span> Contato</span>
